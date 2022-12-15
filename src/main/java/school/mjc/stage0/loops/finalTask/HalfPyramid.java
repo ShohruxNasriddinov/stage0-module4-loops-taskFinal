713865a32af0544be8957a483d4dba1f7a2a81ef
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-15 12:15:31 +0500

    Commit loops taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        if (cathetusLength == 0) {
+            System.out.println("");
+        } else{
+            for (int i = 0; i < cathetusLength; i++) {
+                for (int j = 0; j < cathetusLength - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    }
 
     }
 
